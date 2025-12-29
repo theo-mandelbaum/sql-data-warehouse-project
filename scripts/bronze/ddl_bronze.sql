ee41a572067bbe9b13f266520192c5f5da236ec2
Theo Mandelbaum <161162282+theo-mandelbaum@users.noreply.github.com>
2025-12-28 22:38:50 -0500

    Change TIMESTAMP to DATE for product dates

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -25,8 +25,8 @@ CREATE TABLE bronze.crm_prd_info (
     prd_nm       VARCHAR(50),
     prd_cost     INT,
     prd_line     VARCHAR(50),
-    prd_start_dt TIMESTAMP,
-    prd_end_dt   TIMESTAMP
+    prd_start_dt DATE,
+    prd_end_dt   DATE
 );
 
 DROP TABLE IF EXISTS bronze.crm_sales_details;
@@ -34,9 +34,9 @@ CREATE TABLE bronze.crm_sales_details (
     sls_ord_num  VARCHAR(50),
     sls_prd_key  VARCHAR(50),
     sls_cust_id  INT,
-    sls_order_dt DATE,
-    sls_ship_dt  DATE,
-    sls_due_dt   DATE,
+    sls_order_dt INT,
+    sls_ship_dt  INT,
+    sls_due_dt   INT,
     sls_sales    INT,
     sls_quantity INT,
     sls_price    INT
