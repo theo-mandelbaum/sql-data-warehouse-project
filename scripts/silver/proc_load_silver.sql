63592e2375484a37a8c4be413e30ded2df1d8ab8
Theo Mandelbaum <161162282+theo-mandelbaum@users.noreply.github.com>
2025-12-30 18:21:48 -0500

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -32,3 +32,34 @@ FROM (
 	FROM bronze.crm_cust_info
 	WHERE cst_id IS NOT NULL
 )t WHERE flag_last = 1;
+
+
+
+TRUNCATE TABLE silver.crm_prd_info;
+
+INSERT INTO silver.crm_prd_info (
+	prd_id,
+	cat_id,
+	prd_key,
+	prd_nm,
+	prd_cost,
+	prd_line,
+	prd_start_dt,
+	prd_end_dt
+)
+SELECT 
+	prd_id,
+	REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') AS cat_id,
+	SUBSTRING(prd_key, 7, LENGTH(prd_key)) AS prd_key,
+	prd_nm,
+	COALESCE(prd_cost, 0) as prd_cost,
+	CASE UPPER(TRIM(prd_line))
+		WHEN 'M' THEN 'Mountain'
+		WHEN 'R' THEN 'Road'
+		WHEN 'S' THEN 'other Sales'
+		WHEN 'T' THEN 'Touring'
+		ELSE 'n/a'
+	END AS prd_line,
+	prd_start_dt,
+	LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt)-1 AS prd_end_dt
+FROM bronze.crm_prd_info;
