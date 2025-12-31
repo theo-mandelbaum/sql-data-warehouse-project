62dc5bf83570c1b6a3e7ecd0354e47a9b1c0d81d
Theo Mandelbaum <161162282+theo-mandelbaum@users.noreply.github.com>
2025-12-30 22:48:29 -0500

    Added table erp_loc_a101

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -103,7 +103,7 @@ SELECT
 FROM bronze.crm_sales_details;
 
 
-TRUNCATE silver.erp_cust_az12;
+TRUNCATE TABLE silver.erp_cust_az12;
 
 INSERT INTO silver.erp_cust_az12 (
 	cid,
@@ -122,3 +122,19 @@ SELECT
 		ELSE 'n/a'
 	END as gen
 FROM bronze.erp_cust_az12;
+
+
+TRUNCATE TABLE silver.erp_loc_a101;
+
+INSERT INTO silver.erp_loc_a101 (
+	cid,
+	cntry
+)
+SELECT
+	REPLACE(cid, '-', '') AS cid,
+	CASE WHEN TRIM(cntry) = 'DE' THEN 'Germany'
+		WHEN TRIM(cntry) IN ('US', 'USA') THEN 'United States'
+		WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'n/a'
+		ELSE TRIM(cntry)
+	END AS cntry
+FROM bronze.erp_loc_a101;
