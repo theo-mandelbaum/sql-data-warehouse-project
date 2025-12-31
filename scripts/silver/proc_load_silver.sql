9a6675493211006d67735d1957c8dc90e9859437
Theo Mandelbaum <161162282+theo-mandelbaum@users.noreply.github.com>
2025-12-30 22:26:52 -0500

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -65,7 +65,7 @@ SELECT
 FROM bronze.crm_prd_info;
 
 
-TRUNCATE TABLE silver.sales_details
+TRUNCATE TABLE silver.sales_details;
 
 INSERT INTO silver.crm_sales_details (
 	sls_ord_num,
@@ -102,3 +102,23 @@ SELECT
 	END AS sls_price
 FROM bronze.crm_sales_details;
 
+
+TRUNCATE silver.erp_cust_az12;
+
+INSERT INTO silver.erp_cust_az12 (
+	cid,
+	bdate,
+	gen
+)
+SELECT
+	CASE WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4, LENGTH(cid))
+		ELSE cid
+	END cid,
+	CASE WHEN bdate > NOW() THEN NULL
+		ELSE bdate
+	END AS bdate,
+	CASE WHEN UPPER(TRIM(gen)) in ('F', 'FEMALE') THEN 'Female'
+		WHEN UPPER(TRIM(gen)) in ('M', 'MALE') THEN 'Male'
+		ELSE 'n/a'
+	END as gen
+FROM bronze.erp_cust_az12;
