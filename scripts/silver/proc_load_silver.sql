898185a10afb2b5807216cb52c9c257d5cdcd35a
Theo Mandelbaum <161162282+theo-mandelbaum@users.noreply.github.com>
2025-12-31 19:21:49 -0500

    Adding silver.erp_px_cat_g1v2 table

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -138,3 +138,15 @@ SELECT
 		ELSE TRIM(cntry)
 	END AS cntry
 FROM bronze.erp_loc_a101;
+
+
+TRUNCATE TABLE silver.erp_px_cat_g1v2;
+
+INSERT INTO silver.erp_px_cat_g1v2
+(id, cat, subcat, maintenance)
+SELECT
+	id,
+	cat,
+	subcat,
+	maintenance
+FROM bronze.erp_px_cat_g1v2;
